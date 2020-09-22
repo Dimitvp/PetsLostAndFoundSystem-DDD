1c86203d2620836beca0bd7454fb47d9a05e40c5
Dimitar <dimitar.nikolov.tvp@gmail.com>
2020-09-22 12:35:22 +0300

    Add the initial skeleton of the project.

---
+++ PetsLostAndFoundSystem/Application/Common/Contracts/ICurrentUser.cs
@@ -0,0 +1,7 @@
+﻿namespace PetsLostAndFoundSystem.Application.Common.Contracts
+{
+    public interface ICurrentUser
+    {
+        string UserId { get; }
+    }
+}
---
+++ PetsLostAndFoundSystem/Application/Identity/IUser.cs
@@ -0,0 +1,9 @@
+﻿namespace PetsLostAndFoundSystem.Application.Identity
+{
+    using Domain.Dealerships.Models.Dealers;
+
+    public interface IUser
+    {
+        void BecomeDealer(Dealer dealer);
+    }
+}
